dbcdfdd55016ec8183410a95251343a28de4b416
GMMarshall <gmmarshall27@gmail.com>
2021-11-11 11:41:05 -0500

    finished get range route

--- routes/api.js
+++ routes/api.js
@@ -17,3 +17,19 @@ WORKOUT.Workout.aggregate([
 .then((workout)=> res.json(workout))
 .catch((err) => res.status(400).json(err))
 );
+
+router.get("/api/workouts/range/", ({}, res)=>
+WORKOUT.Workout.aggregate([
+    {
+        $addFields: {
+            totalDuration: { $sum: "$exercises.duration"},
+            totalDistance: { $sum:"$exercises.distance"},
+            totalSets: { $sum: "$exercises.sets"},
+            totalReps: { $sum:"$exercises.reps"},
+            totalWeight: { $sum:"$exercises.weight"},
+        },
+    },
+])
+.then((workout)=> res.json(workout))
+.catch((err) => res.status(400).json(err))
+);
