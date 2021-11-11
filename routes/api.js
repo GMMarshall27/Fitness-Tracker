2cc914b82345b6b03a9b662a7bf10cb3b03979f2
GMMarshall <gmmarshall27@gmail.com>
2021-11-11 12:04:44 -0500

    added route for stats html

--- routes/api.js
+++ routes/api.js
@@ -51,3 +51,9 @@ WORKOUT.Workout.findByIdAndUpdate(
 router.get("/exercise", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/exercise.html"))
 );
+
+router.get("/stats", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/stats.html"))
+);
+
+module.exports = router;
