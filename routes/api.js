b80a6c7cdfe5add3dc3bf72b755577c926571dae
GMMarshall <gmmarshall27@gmail.com>
2021-11-11 11:35:29 -0500

    finished get workouts route

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
-const db = require("../models/index.js");
+const WORKOUT = require("../models/index.js");
 const path = require("path");
 
 router.get("/api/workouts/", (req,res)=>
-db.Workout.aggregate([
+WORKOUT.Workout.aggregate([
     {
         $addFields: {
             totalDuration: { $sum:"$exercises.duration"},
-        }
-    }
+            totalDistance: { $sum: "$exercises.distance"},
+            totalSets: { $sum:"$exercises.sets"},
+            totalReps: { $sum:"$exercises.reps"},
+            totalWeight: { $sum:"$exercises.weight"},
+        },
+    },
 ])
-)
+.then((workout)=> res.json(workout))
+.catch((err) => res.status(400).json(err))
+);
